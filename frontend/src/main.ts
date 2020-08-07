d8bcfed423830deb369208981d0d8bd2fb7ad724
Ion Cretu <cretuion94@gmail.com>
2020-08-07 18:35:51 +0300

    fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import i18n from "./plugins/i18n";
-import vuetify from "./plugins/vuetify";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
